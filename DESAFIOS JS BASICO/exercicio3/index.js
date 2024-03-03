5d544a092d1299923732faee3c289d93c392c30f
Jefferson Silva <jeffersonsilva2013.jf@gmail.com>
2024-03-03 00:56:47 -0400

    Resolução do exercício 4 usando laço de repetição for

---
+++ DESAFIOS JS BASICO/exercicio3/index.js
@@ -0,0 +1,28 @@
+const desejaSeHospedar = confirm(
+  "Gostaria de pagar R$ 50,00 para passar a noite em nossa hospedagem ?"
+);
+
+if (desejaSeHospedar) {
+  alert("Ótimo! Nós temos as melhores camas de toda a região!");
+} else {
+  alert("Que pena! Você parecia ser uma pessoa mais legal!");
+}
+
+// Minha resolução comentada
+
+// let nome = prompt("Olá qual o seu nome aventureiro(a)?");
+
+// const opcao = prompt(
+//   "Seja bem vindo(a) " +
+//     nome +
+//     ", Gostaria de pagar R$ 50,00 para passar a noite em nossa hospedagem ?" +
+//     " Escolha uma opção abaixo:\n1 - Sim\n2 - Não"
+// );
+
+// if (opcao === "1") {
+//   alert("Ótimo! Nós temos as melhores camas de toda a região!");
+// } else if (opcao === "2") {
+//   alert("Que pena! Você parecia ser uma pessoa mais legal!");
+// } else {
+//   alert("Opção inválida!");
+// }
---
+++ DESAFIOS JS BASICO/exercicio4/index.js
@@ -0,0 +1,8 @@
+let i = 0;
+
+for (let i = 0; i <= 10; i++) {}
+
+while (i <= 10) {
+  alert("o numero processado é : " + i);
+  i++;
+}
