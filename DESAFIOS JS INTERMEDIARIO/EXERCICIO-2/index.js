c8be98fe65065037225acbacfa15752962872c0e
Jefferson Silva <jeffersonsilva2013.jf@gmail.com>
2024-03-12 22:47:46 -0400

    Resolução do exercício 2 usando forEach

---
+++ DESAFIOS JS INTERMEDIARIO/EXERCICIO-2/index.js
@@ -0,0 +1,10 @@
+const meuArray = [65, 44, 12, 4];
+meuArray.forEach(function (elemento) {
+  console.log(elemento * 10);
+});
+
+/* RESPOSTA :
+const numeros = [65, 44, 12, 4];
+numeros.forEach((item) => {
+  console.log(item * 10);
+});*/
